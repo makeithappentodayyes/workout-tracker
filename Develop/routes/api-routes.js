e061f44db2f38917bda768c368c165de5d7e6e5d
Daniel Gil <danielgilucsd@gmail.com>
2020-02-04 01:21:52 -0800

    api route to create a new workout added

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -27,3 +27,27 @@ router.put("/api/workouts/:id", (req, res) => {
       res.status(400).json(err);
     });
 });
+
+//create a  workout
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find()
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
